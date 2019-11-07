d8d055a25509dbf6d07ad400ae14ebf792c1306c
shengwen-xue <xueshengwen@aliyun.com>
2019-11-07 09:39:27 +0800

    init object

---
+++ src/main/java/com/example/demo/demo/TestDemo.java
@@ -0,0 +1,12 @@
+package com.example.demo.demo;
+
+/**
+ * Create by
+ *
+ * @author xueshengwen
+ */
+public class TestDemo {
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
